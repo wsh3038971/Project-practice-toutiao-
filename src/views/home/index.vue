5675c7dc0d1e24bf8e8aa28a37e17b538ffbe2d2
王煜 <964934202@qq.com>
2019-11-12 21:41:16 +0800

    home页结构搭建完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>home</div>
+  <!-- 最外层容器 -->
+  <el-container>
+    <!-- 左侧 -->
+    <el-aside>左侧</el-aside>
+    <!-- 右侧整体 -->
+    <el-container>
+      <!-- 右侧头部 -->
+      <el-header>右侧头部</el-header>
+      <!-- 右侧内容部分 -->
+      <el-main>右侧内容部分</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
