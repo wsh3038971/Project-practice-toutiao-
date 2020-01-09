5bcf1acc1b56daa5a04f5063bd94186d5dcf2a87
王煜 <964934202@qq.com>
2020-01-09 14:34:36 +0800

    修改用户信息完成

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,12 +5,80 @@
                 用户信息
             </template>
         </bread-crumb>
+        <el-form ref="userForm" :model="userInfo" :rules="userRules" label-width="100px">
+            <el-form-item label="用户名" prop="name">
+                <el-input v-model="userInfo.name" style="width: 300px"></el-input>
+            </el-form-item>
+            <el-form-item label="简介" prop="intro">
+                <el-input v-model="userInfo.intro" style="width: 300px"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+                <el-input v-model="userInfo.email" style="width: 300px"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号">
+                <el-input v-model="userInfo.mobile" style="width: 300px" disabled></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="saveUserInfo">保存</el-button>
+            </el-form-item>
+        </el-form>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      },
+      userRules: {
+        name: [{
+          required: true,
+          message: '用户名不能为空'
+        }, {
+          min: 2,
+          max: 8,
+          message: '用户名为2-8之间'
+        }],
+        email: [{
+          pattern: /^[A-Za-zd0-9]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$/,
+          message: '邮箱格式不正确'
+        }]
+      }
+    }
+  },
+  methods: {
+    // 保存按钮
+    saveUserInfo: function () {
+      this.$refs.userForm.validate((isOk) => {
+        if (isOk) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.userInfo
+          }).then(res => {
+            this.$message({ type: 'success', message: '修改成功' })
+          })
+        }
+      })
+    },
+    // 初始数据
+    getUserInfo: function () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(res => {
+        this.userInfo = res.data
+      })
+    }
+  },
+  created: function () {
+    this.getUserInfo()
+  }
 }
 </script>
 
