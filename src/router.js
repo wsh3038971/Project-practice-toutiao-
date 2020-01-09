df467e9a81a8cc6a5d542d676a20c8cc45f7ef52
王煜 <964934202@qq.com>
2020-01-09 11:50:50 +0800

    用户信息页面路由挂载

--- src/router.js
+++ src/router.js
@@ -38,6 +38,9 @@ export default new Router({
       {
         path: 'publish/:articleId',
         component: () => import('./views/publish')
+      }, {
+        path: 'account',
+        component: () => import('./views/account')
       }]
     }
     // {
